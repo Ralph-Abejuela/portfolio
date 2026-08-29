8f8c8bc9b5f9c950dbd9c38a8eed7f978ea1cfde
ralph-abejuela <abejuela.ralph.balatucan@gmail.com>
2026-08-29 14:44:36 +0800

    feat(react): add @astrojs/react integration for islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ralphabejuela.com",
@@ -19,5 +21,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(), react()],
 });
